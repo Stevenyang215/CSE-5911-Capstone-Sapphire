5694e21a3cfbfaa20bd5ed9121aced8da06e4344
Steven Yang <634539140@qq.com>
2016-02-08 15:37:41 -0500

    Add activity for capturing video by camera in the system

---
+++ app/src/main/java/sapphire/seemetrain/VideoCapture.java
@@ -0,0 +1,79 @@
+package sapphire.seemetrain;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.net.Uri;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class VideoCapture extends AppCompatActivity {
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    private Button record_button;
+    private Button play_button;
+    private VideoView videoView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_capture);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        record_button = (Button) findViewById(R.id.RECORD_BUTTON);
+        play_button = (Button) findViewById(R.id.PLAY_BUTTON);
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        record_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent captureVideo = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if(captureVideo.resolveActivity(getPackageManager())!=null){
+                        startActivityForResult(captureVideo,REQUEST_VIDEO_CAPTURE);
+                    }
+            }
+        });
+
+        play_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               videoView.start();
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK){
+            Uri video = data.getData();
+            videoView.setVideoURI(video);
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
