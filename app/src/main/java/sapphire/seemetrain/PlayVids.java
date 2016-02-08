1d5a2605db7768e3df340dca22c47a20f6add7e9
joedonnelly9 <joedonnelly92@gmail.com>
2016-02-08 12:20:57 -0500

    added some permissions requests and added class for Play Video screen

---
+++ app/src/main/java/sapphire/seemetrain/PlayVids.java
@@ -0,0 +1,23 @@
+package sapphire.seemetrain;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+/**
+ * Created by Joseph on 2/7/16.
+ */
+public class PlayVids extends FragmentActivity implements View.OnClickListener{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.playvid);
+
+        //TODO finish, create layout.playvid
+    }
+
+    public void onClick(View v) {
+        //TODO
+    }
+
+}
