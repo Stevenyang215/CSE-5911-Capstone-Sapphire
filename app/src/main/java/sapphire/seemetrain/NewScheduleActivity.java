94859955c81751a3d560764bb0a018df14006b94
Steven Yang <634539140@qq.com>
2016-02-17 16:21:26 -0500

    update the new schedule and remove the button for addVideo

--- app/src/main/java/sapphire/seemetrain/NewScheduleActivity.java
+++ app/src/main/java/sapphire/seemetrain/NewScheduleActivity.java
@@ -13,25 +13,22 @@ import android.widget.Button;
 public class NewScheduleActivity extends AppCompatActivity {
 
     private TextView title;
-    private Button add;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_schedule);
         title = (TextView) findViewById(R.id.new_schedule);
-        add = (Button) findViewById(R.id.add);
-
-
-    }
-
-    public void addVideo(View view) {
+        //add = (Button) findViewById(R.id.add);
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         addVideoFragment add_Video_Fragment = new addVideoFragment();
         fragmentTransaction.replace(R.id.fragment_container, add_Video_Fragment);
         fragmentTransaction.commit();
 
+
     }
+
 
 }
